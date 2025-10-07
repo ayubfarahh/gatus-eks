af391b9eb0a3132ddf586d20ad50660a35c06dad
ayubfarahh <ayubfarahh@gmail.com>
2025-10-07 23:18:37 +0100

    Added worker nodes to infra

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,12 +1,8 @@
 resource "aws_eks_cluster" "gatus-cluster" {
-  name = "example"
-
-  access_config {
-    authentication_mode = "API"
-  }
-
+  name = "gatus-controlplane"
   role_arn = aws_iam_role.cluster.arn
-  version  = "1.31"
+  version  = "1.33"
+  
 
   vpc_config {
     subnet_ids = var.priv_subnets
@@ -42,4 +38,59 @@ resource "aws_iam_role" "cluster" {
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.cluster.name
+}
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.gatus-cluster.name
+  node_group_name = "worker-nodes"
+  node_role_arn   = aws_iam_role.workers.arn
+  subnet_ids      = var.priv_subnets
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.worker-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.worker-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.worker-AmazonEKSWorkerNodePolicy,
+  ]
+}
+
+resource "aws_iam_role" "workers" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "worker-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.workers.name
+}
+
+resource "aws_iam_role_policy_attachment" "worker-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.workers.name
+}
+
+resource "aws_iam_role_policy_attachment" "worker-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.workers.name
 }
